ec4c05a523576c7cdcd70282581300e7160d8b26
denpronin <pronin_d@yahoo.com>
2020-12-22 18:37:00 +0300

    Changes after code review

--- app/src/main/java/com/example/parsehttp/MainActivity.java
+++ app/src/main/java/com/example/parsehttp/MainActivity.java
@@ -1,56 +1,102 @@
 package com.example.parsehttp;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 import android.text.method.ScrollingMovementMethod;
+import android.util.Log;
 import android.view.View;
 import android.widget.EditText;
 import android.widget.TextView;
 
-import java.io.BufferedInputStream;
 import java.io.BufferedReader;
 import java.io.IOException;
-import java.io.InputStream;
 import java.io.InputStreamReader;
-import java.net.HttpURLConnection;
-import java.net.MalformedURLException;
 import java.net.URL;
 
 import javax.net.ssl.HttpsURLConnection;
 
 public class MainActivity extends AppCompatActivity {
-    EditText editText;
-    TextView textView;
+    private EditText textURL;
+    private TextView textResponse;
+    private static final String TAG = "MainActivity";
+    public static final String RESPONSE_TEXT = "textResponse";
+
+    @Override
+    public void onSaveInstanceState(@NonNull Bundle outState) {
+        super.onSaveInstanceState(outState);
+        outState.putString(RESPONSE_TEXT, textResponse.getText().toString());
+
+
+    }
+
+    @Override
+    protected void onRestoreInstanceState(@NonNull Bundle savedInstanceState) {
+
+        super.onRestoreInstanceState(savedInstanceState);
+
+        textResponse.setText(savedInstanceState.getString(RESPONSE_TEXT, "000"));
+
+    }
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        textURL = (EditText) findViewById(R.id.url_text);
+        textResponse = (TextView) findViewById(R.id.request_text);
+        textResponse.setMovementMethod(new ScrollingMovementMethod());
+
     }
 
     public void onButtonClick(View view) {
-        editText = (EditText) findViewById(R.id.url_text);
-        textView = (TextView) findViewById(R.id.request_text);
-        textView.setMovementMethod(new ScrollingMovementMethod());
-        textView.setText("");
+
+
+        textResponse.setText("");
+
         Thread request = new Thread(new Runnable() {
             @Override
             public void run() {
+                /*try {
+                    Thread.sleep(5000);
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }*/
+                HttpsURLConnection urlConnection;
+                BufferedReader reader = null;
+                InputStreamReader inputStream = null;
                 try {
-                    URL url = new URL(editText.getText().toString());
-                    HttpsURLConnection urlConnection = (HttpsURLConnection) url.openConnection();
-                    InputStreamReader inputStream = new InputStreamReader(urlConnection.getInputStream());
-                    BufferedReader reader = new BufferedReader(inputStream);
+                    URL url = new URL(textURL.getText().toString());
+                    urlConnection = (HttpsURLConnection) url.openConnection();
+                    inputStream = new InputStreamReader(urlConnection.getInputStream());
+                    reader = new BufferedReader(inputStream);
                     CharSequence line;
                     while ((line = reader.readLine()) != null) {
-                        System.out.println(line);
-                        textView.append(line + "\n");
+                        Log.d(TAG, line.toString());
+                        textResponse.append(line + "\n");
                     }
-                    reader.close();
+
                 } catch (IOException ex) {
                     ex.printStackTrace();
                 }
+
+                finally {
+                    try {
+                        if (inputStream != null) {
+                            inputStream.close();
+                        }
+                        if (reader != null) {
+
+                            reader.close();
+                        }
+                    } catch (IOException e) {
+                        e.printStackTrace();
+                    }
+
+                }
             }
+
         });
         request.start();
     }
